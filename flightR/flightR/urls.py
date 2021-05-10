b9f885bc66dc8f21edd0a2238e5184e222bd7611
Ashish R. Marathe <maratheashish01@gmail.com>
2021-05-10 12:28:46 +0530

    made endpoints i.e urls

--- flightR/flightR/urls.py
+++ flightR/flightR/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from fapp import  views
+router=DefaultRouter() # made instance
+router.register('flight',views.Flight)
+router.register('passenger',views.Flight)
+router.register('reservation',views.Flight)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('flightServices',include(router.urls))
 ]
