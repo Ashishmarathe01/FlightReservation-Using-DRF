1dba619df202509224ba8fa1ad1dcdeb5f98beb7
Ashish R. Marathe <maratheashish01@gmail.com>
2021-05-10 11:57:00 +0530

    model is created

--- flightR/fapp/models.py
+++ flightR/fapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    flightNumber=models.CharField(max_length=10)
+    operatingAirlines=models.CharField(max_length=20)
+    departureCity=models.CharField(max_length=20)
+    arrivalCity=models.CharField(max_length=20)
+    dateOfDeparture=models.DateField()
+    estimatedTimeOfDeparture=models.TextField()
+
+
+
+class Passenger(models.Model):
+    firstName=models.CharField(max_length=20)
+    lastName=models.CharField(max_length=20)
+    middleName=models.CharField(max_length=20)
+    email=models.CharField(max_length=20)
+    phone=models.CharField(max_length=13)
+
+
+
