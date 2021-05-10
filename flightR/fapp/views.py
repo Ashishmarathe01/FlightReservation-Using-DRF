1aa228331d1714a7a5161803a432891a6af0a0ac
Ashish R. Marathe <maratheashish01@gmail.com>
2021-05-10 12:18:18 +0530

    made viewSets

--- flightR/fapp/views.py
+++ flightR/fapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import  Flight,Reservation,Passenger
+from .serializers import FlightSerializer,ReservationSerializer,PassengerSerializer
+from rest_framework import viewsets# for minimizing code it will perform all key and non-key operations
+
 
 # Create your views here.
+class FlightViewSet(viewsets.ModelViewSet): # it will handel all pkey and nonPkey operations
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+class PassengerViewSet(viewsets.ModelViewSet):
+    queryset = Passenger.objects.all()
+    serializer_class = ReservationSerializer
