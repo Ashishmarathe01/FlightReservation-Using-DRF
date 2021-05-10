1439553044e316e13d24924d71694bbc02dfc1c4
Ashish R. Marathe <maratheashish01@gmail.com>
2021-05-10 18:04:46 +0530

    made custome validation

--- flightR/fapp/serializers.py
+++ flightR/fapp/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 from .models import Flight,Reservation,Passenger
+import re
 
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model=Flight
         fields='__all__'
+
+    def validate_flightNumber(self,flightNumber)   :
+        if (re.match("^[a-zA-Z0-9]*$",flightNumber)==None):
+            raise serializers.ValidationError("chek your data")
+        return flightNumber    
 
 
 class ReservationSerializer(serializers.ModelSerializer):
