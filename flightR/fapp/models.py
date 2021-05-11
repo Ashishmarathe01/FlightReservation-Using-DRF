4504de1e03616fa5efabed5a22c3989736f4c387
Ashish R. Marathe <maratheashish01@gmail.com>
2021-05-11 11:53:27 +0530

    automatic token generation done in model just add one function using singal

--- flightR/fapp/models.py
+++ flightR/fapp/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+# importing for atumatic token genertaion
+from  django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
 
 # Create your models here.
 class Flight(models.Model):
@@ -22,5 +27,11 @@ class Reservation(models.Model):
     flight=models.ForeignKey(Flight,on_delete=models.CASCADE)
     passenger=models.OneToOneField(Passenger,on_delete=models.CASCADE)
 
+
+# for atomatic genertion of token
+@receiver(post_save,sender=settings.AUTH_USER_MODEL)
+def creatAuthToken(sender,instance,created,**kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 
